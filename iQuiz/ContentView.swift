fa6ce362cf21542999b8e2941e8fd27f47d2e7c4
Ryan Lee <ralee478.school@gmail.com>
2021-05-05 18:13:01 -0700

    first table cell finished

--- iQuiz/ContentView.swift
+++ iQuiz/ContentView.swift
@@ -7,15 +7,44 @@
 
 import SwiftUI
 
+//var image: String = "function"
+//var titleString: String = "Mathematics"
+//var bodyDesc: String = "Contains quizzes for Mathematics grades K-12"
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TableCell()
+        }
     }
 }
 
+struct TableCell: View {
+    
+    var image: String = "function"
+    var titleString: String = "Mathematics"
+    var bodyDesc: String = "Contains quizzes for Mathematics grades K-12"
+    
+    var body: some View {
+        HStack(spacing: 50) {
+            Image(systemName: image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 70, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            VStack(alignment: .leading) {
+                Text(titleString)
+                    .bold()
+                Text(bodyDesc)
+            }
+        }
+        .padding()
+    }
+}
+
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
